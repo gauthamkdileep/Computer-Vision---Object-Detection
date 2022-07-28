13d8153a6dc9c14c06b5e81bb8597e2cebaf7ed8
gauthamkdileep <gauthamkdileep@gmail.com>
2022-07-28 23:57:30 +0530

    Add files via upload

---
+++ utilities.js
@@ -0,0 +1,32 @@
+export const drawRect = (detections, ctx) =>{
+    detections.forEach(prediction=>{
+      //Get prediction results
+      const [x,y,width,height] = prediction['bbox'];
+      const text = prediction['class'];      
+      var itemofInterest=""; 
+      var prevDetection="";
+      if(text!=="person"){
+        itemofInterest = text;
+
+      }
+
+      if(itemofInterest==prevDetection){
+        
+      }
+      console.log(itemofInterest);
+      //Set styling
+      const color = 'green'
+      ctx.strokeStyle = color
+      ctx.font = '18px Arial'
+      ctx.fillStyle = color
+
+
+      //Draw rectangles and text
+      ctx.beginPath()
+      ctx.fillText(text, x, y)
+      ctx.rect(x, y, width, height)
+      ctx.stroke()
+
+
+    })
+}
